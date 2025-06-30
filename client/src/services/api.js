89941934a7b75989067932a3584caaea9d30c9a4
MithunkarthikK <k.k.mithunkarthik@gmail.com>
2025-06-30 22:54:14 +0530

    Testing

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
-
-const API = axios.create({
-  baseURL: 'https://silentvote-backend.onrender.com/api',
-});
+const API = axios.create({ baseURL: 'http://localhost:5000/api' });
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
   if (token) req.headers.Authorization = `Bearer ${token}`;
